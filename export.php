1d37412e2b52ae904ec47c99bb253da8142560aa
Alejandro Roa <ingjesusroa@gmail.com>
2018-06-26 15:16:15 -0400

    output to browser

--- export.php
+++ export.php
@@ -1,16 +1,40 @@
 
 <?php 
 
+function storage(){
+
 	$fp = fopen('file.csv', 'w');
 
 	foreach ($data as $d) { 
-      $list = array ( array($item1, ";", $item2) );
+    	$list = array ( array($data->item1, ";", $data->item2) );
 
-      foreach ($list as $fields) {
-	   	   fputcsv($fp,$fields, chr(0));  //chr0 escapes special characters
-		  }
+    	foreach ($list as $fields) {
+	   		fputcsv($fp,$fields, chr(0));  //chr0 escapes special characters
+		}
 	}
 
 	fclose($fp);
+}
+
+
+
+function outputbrowser{
+	header("Content-type: text/csv");
+    header("Cache-Control: no-store, no-cache");
+    header('Content-Disposition: attachment; filename="filename.csv"');
+
+    $json = getCustomers("json");
+    $outstream = fopen("php://output",'w');
+
+    foreach ($data as $d) { 
+    	$list = array ( array($data->item1, ";", $data->item2) );
+
+    	foreach ($list as $fields) {
+	   		fputcsv($outstream,$fields, chr(0));  //chr0 escapes special characters
+		}
+	}
+
+   fclose($outstream);
+}
 
 ?>
